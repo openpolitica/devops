731b8ccf82c931fd862edfe8181854e6c4fac31f
Luighi <luighiavz@gmail.com>
2021-02-25 22:21:04 -0500

    Add files to build container image

--- deploy-backend.sh
+++ deploy-backend.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -x
 
+#Init directory 
+INIT_DIR=${PWD}
+
 #Setting variables
 if [ -z "$1" ]
   then
@@ -13,5 +16,16 @@ cd ${SERVICES_DIRECTORY}
 git clone https://github.com/openpolitica/open-politica-backend.git
 cd open-politica-backend
 git checkout tags/v1.0 -b 1.0
+
+#Add configuration files for backend
+cp ${INIT_DIR}/backend/Dockerfile ./
+cp ${INIT_DIR}/backend/docker-compose ./
+
+if [ -z $HOST_DOMAIN ] || [ -z $EMAIL_DOMAIN ]; then
+  echo "Environment variables not set, loading .env file"
+  cp ${INIT_DIR}/backend/.env ./
+fi
+
+sudo docker build -t openpolitica/votu_backend:latest -f Dockerfile . 
 
 sudo docker-compose up -d
