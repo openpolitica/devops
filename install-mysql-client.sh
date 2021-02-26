83888f505adf05254e060e0f6fbf3fb6198e6244
Luighi <luighiavz@gmail.com>
2021-02-25 23:01:31 -0500

    Add mysql client installer

---
+++ install-mysql-client.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+
+#Verify if java exist, if not install it
+if ! command -v mysql &> /dev/null
+then
+	#Installing java
+	sudo apt-get update
+	sudo apt-get install -y mysql-client
+else 
+	echo "Java is installed in your system... Skipping"
+fi
