e84f1302b061cdfffc4d9c45b0d29b901e4d5770
Luighi <luighiavz@gmail.com>
2021-03-03 00:36:53 -0500

    Upload backup script

---
+++ database/upload-backup.sh
@@ -0,0 +1,19 @@
+#/bin/bash -x
+
+DATABASE_BACKUP_FILEPATH=${INIT_DIR}/database.sql
+# Check if there is a backup in path
+if [ !  -f "$DATABASE_BACKUP_FILEPATH" ];then
+  echo "Backup file doesn't found in directory, skipping"
+  exit 1
+fi
+
+git add $DATABASE_BACKUP_FILEPATH
+git commit -m "Update database"
+
+if [ -z $GIT_USER ] || [ -z $GIT_API ]; then
+  echo "Environment variables not set, you should pass in prompt"
+  git push -u origin database-update
+else
+  git push --repo https://$GIT_USER:$GIT_API@github.com/openpolitica/devops.git database-update
+fi
+
