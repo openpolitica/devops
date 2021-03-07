f98055d352fef56eab34e145355e4d2d0429d565
Luighi <luighiavz@gmail.com>
2021-03-07 18:11:26 -0500

    Fix mysql client version syntax

--- database/create-backup.sh
+++ database/create-backup.sh
@@ -12,7 +12,7 @@ DATABASE_BACKUP_NAME=database.back.sql
 #Create a copy for a modified database
 #Column-statistics is disabled, otherwise will throw an error
 VERSION=`mysqldump --version | awk '{ print $3}' | awk 'BEGIN{FS="."} {print $1}'`
-if [ $VERSION=='8' ]; then
+if [ $VERSION == '8' ]; then
   mysqldump --skip-opt --column-statistics=0 --user=$MYSQL_USER --password=$MYSQL_PWD --host=$MYSQL_HOST --databases $DATABASE_NAME > $DATABASE_BACKUP_NAME
 else
   mysqldump --skip-opt --user=$MYSQL_USER --password=$MYSQL_PWD --host=$MYSQL_HOST --databases $DATABASE_NAME > $DATABASE_BACKUP_NAME
